b90201ffea39276ea9143e0ba434e2f1e5f24da3
Harsh Sharma <harsh.mhs@yahoo.com>
2024-07-08 22:00:06 +1000

    edited frontend for users

--- NyxAcademy/NyxAcademy/settings.py
+++ NyxAcademy/NyxAcademy/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'nyxacademy', 
         'USER': 'postgres',
-        'PASSWORD': 'Harsh1410',
+        'PASSWORD': '#',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
