43ea87be9bfd5161e75bb5872e7f0698a88d3eec
Harsh Sharma <harsh.mhs@yahoo.com>
2024-07-08 20:52:12 +1000

    added user auth and requirments.txt

--- NyxAcademy/NyxAcademy/settings.py
+++ NyxAcademy/NyxAcademy/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'NyxAcademy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nyxacademy', 
+        'USER': 'postgres',
+        'PASSWORD': 'Harsh1410',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
@@ -126,7 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static', 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -143,3 +157,4 @@ LOGIN_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
+
