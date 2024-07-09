1495613c0233f1b09772195a1e354cd4daa10f69
Pulkit-thinknyx <pulkit.khandelwal@thinknyx.com>
2024-07-09 11:38:45 +0530

    course model created

--- NyxAcademy/users/models.py
+++ NyxAcademy/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    creator = models.CharField(max_length=100)
+    image = models.TextField()
+    link = models.TextField()
+    def __str__(self):
+        return self.name
