da35364604085617dd6f35d600493f8cdc1e3ee2
Pulkit-thinknyx <pulkit.khandelwal@thinknyx.com>
2024-07-09 10:55:59 +0530

    bugs fixed

--- NyxAcademy/NyxAcademy/settings.py
+++ NyxAcademy/NyxAcademy/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'nyxacademy', 
         'USER': 'postgres',
-        'PASSWORD': '#',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
